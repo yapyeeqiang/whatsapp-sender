6aebf7c0ca2137a087605975e9cf6b74dd197010
yapyeeqiang <yapyeeqiang@gmail.com>
2021-12-31 18:19:03 +0800

    ✨ skip invalid contact and continue running the task

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from utils.file import read_file
 from utils.whatsapp import send_message
+import os
+
+cwd = os.path.abspath(os.getcwd())
 
 # Get messages
-message = read_file("message.txt")
+message = read_file(f"{cwd}/assets/message.txt")
 
 # Get all contacts
-contacts = read_file("contacts.txt", array=True)
+contacts = read_file(f"{cwd}/assets/contacts.txt", array=True)
 
 # Initialize Chrome Driver
 s = Service(ChromeDriverManager().install())
