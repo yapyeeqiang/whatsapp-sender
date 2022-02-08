7d91b749abf674c7c38515ebe2139b5ab6076acf
yapyeeqiang <yapyeeqiang@gmail.com>
2022-02-08 17:48:56 +0800

    feat: headless

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from utils.file import read_file
 from utils.whatsapp import send_message
 import os
+
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument(r"user-data-dir=./driver/cache")
+chrome_options.add_argument( "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36")
+chrome_options.add_argument("remote-debugging-port=3333")
 
 cwd = os.path.abspath(os.getcwd())
 
@@ -15,7 +22,7 @@ contacts = read_file(f"{cwd}/assets/contacts.txt", array=True)
 
 # Initialize Chrome Driver
 s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
 
 # Send WhatsApp Message
 send_message(driver=driver, contacts=contacts, message=message)
--- utils/whatsapp.py
+++ utils/whatsapp.py
@@ -1,17 +1,15 @@
 from urllib.parse import quote
 from time import sleep
-from .phone_number import is_phone_valid
 from .file import write_file, clean_file
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import os
-# from termcolor import colored
 
 cwd = os.path.abspath(os.getcwd())
 
 WHATSAPP_WEB_URL = 'https://web.whatsapp.com'
-DELAY = 30
+DELAY = 50
 
 
 def print_empty_lines(n):
@@ -36,25 +34,28 @@ def send_message(driver, contacts, message):
 
     driver.get(WHATSAPP_WEB_URL)
 
-    input("Press any key to start...")
+    # Wait for chats to be visible
+    start = False
 
+    try:
+        while not start:
+            element = driver.find_element_by_tag_name('progress')
+
+            if not element:
+                start = True
+    except Exception as e:
+        pass
+    
+    
     contacts = [x for x in contacts if x]
 
     clean_file(f"{cwd}/assets/invalid_contacts.txt")
 
     for index, contact in enumerate(contacts):
-        isPhoneValid = is_phone_valid(contact)
 
         sending_index = index + 1
         total_contacts = len(contacts)
 
-        # if not isPhoneValid:
-        #     print(f"{sending_index}/{total_contacts} => Phone number is invalid.")
-        #     print_empty_lines(1)
-
-        #     write_file(f"{cwd}/assets/invalid_contacts.txt", contact)
-
-        #     continue
 
         if contact == "":
             continue
@@ -80,22 +81,18 @@ def send_message(driver, contacts, message):
                     write_file(f"{cwd}/assets/invalid_contacts.txt", contact)
 
                     continue
-                    # print(f"Failed to send message to this contact: {contact}, retry ({sending_index}/3)")
-                    # print_empty_lines(1)
-                    # print("Make sure your phone and computer are connected to the internet.")
-                    # print("If there is an alert, please dismiss it.")
-                    # print_empty_lines(1)
-                    # input("Press any key to continue...")
-                else:
-                    sleep(1)
+            else:
+                sleep(1)
 
-                    send_button.click()
-                    message_sent = True
+                print('hehe')
 
-                    sleep(3)
+                send_button.click()
+                message_sent = True
 
-                    print(f"Message sent to: {contact}")
-                    print_empty_lines(1)
+                sleep(3)
+
+                print(f"Message sent to: {contact}")
+                print_empty_lines(1)
 
         except Exception as e:
             print(f"Failed to send message to {contact}: {str(e)}")
